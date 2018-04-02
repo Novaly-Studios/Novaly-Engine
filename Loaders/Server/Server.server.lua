a9302d126c7f78c14d25e0a420028fbf25306c99
Adam Mabin <adam.mabin@googlemail.com>
2018-04-03 00:50:14 +0100

    Changes for testing

--- Loaders/Server/Server.server.lua
+++ Loaders/Server/Server.server.lua
@@ -5,14 +5,13 @@ setfenv(1, Func())
 repeat wait() until Players:GetChildren()[1]
 local Player = Players:GetChildren()[1]
 
---[[PlayerDataManagement.WaitForPlayerData(Player)
+PlayerDataManagement.WaitForPlayerData(Player)
 
 PlayerData[tostring(Player.UserId)] = {
-	Check = 1;
-	Money = 3000;
-	Ayy = {
-		Value = "Str";
-		Numeric = 353425.124;
-	};
+    Check = 1;
+    Money = 3000;
+    Ayy = {
+        Value = "Str";
+        Numeric = 353425.124;
+    };
 }
-]]
