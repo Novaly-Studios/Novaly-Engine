c897e255cf2e610d5795448c1ff00ca02480a4f5
Adam Mabin <amabin@roblox.local>
2018-07-05 13:31:01 -0700

    Reformatting

--- Source/Novarine.lua
+++ Source/Novarine.lua
@@ -8,27 +8,17 @@ local Loaded                = false
 
 local Environment           = {}
 local EnvironmentMT         = {
-    
     __index = function(Self, Key)
-        
         return Environment[Key] or rawget(Self, 1)[Key]
-        
     end;
-    
 }
 
 if Server then
-
     Environment["Assets"] = ReplicatedStorage:FindFirstChild("Assets") or Instance.new("Folder", ReplicatedStorage)
-
 else
-
     coroutine.resume(coroutine.create(function()
-
         Environment["Assets"] = ReplicatedStorage:WaitForChild("Assets")
-
     end))
-
 end
 
 Environment["Modules"]          = ReplicatedStorage.Modules
@@ -41,23 +31,15 @@ local function AddPlugin(Plugin)
     local Object = (Server and Plugin.Server or Plugin.Client)
     
     if Object["__main"] then
-
         Object["__main"]()
-
     end
 
     for Key, Value in next, Object do
-
         if Key ~= "__main" then
-            
             if Environment[Key] then
-
                 print("\tWarning: Library item '" .. Key .. "' being overwritten.")
-
             end
-
             Environment[Key] = Value
-            
         end
     end
 end
@@ -65,34 +47,20 @@ end
 return function(Value)
 
     if Value then
-
         local TypeStr = type(Value)
-
         if TypeStr == "string" then
-
             if Value:lower() == "wait" then
-
                 while Loaded == false do
-                    
                     wait()
-
                 end
-
             elseif Value:lower() == "complete" then
-
                 Loaded = true
-
             end
-
         elseif TypeStr == "table" then
-            
             AddPlugin(Value)
             return
-            
         end
-
     end
 
     return setmetatable({getfenv(0)}, EnvironmentMT)
-    
 end
